eaacdc78c3923626abe348750a5bf2d62ef2381b
heynaveed <naveedpasha0058@gmail.com>
2022-11-09 23:16:56 +0530

    Completed endpoints for men and women

---
+++ Backend/Models/Products.model.js
@@ -0,0 +1,34 @@
+const mongoose=require("mongoose");
+
+// SCHEMA FOR MENS DOCUMENTS
+const MenSchema= new mongoose.Schema({
+    col_sm_4href:String,
+    productImgTagsrc:String,
+    plp:String,
+    clr_shade4:String,
+    wishlist_icon_animatesrc:String,
+    actualPriceText:String,
+    loyaltyPriceBox2:String,
+    clr_shade_3:String
+})
+// MEN MODEL
+const ProductModelMen= mongoose.model("men",MenSchema);
+
+
+// SCHEMA FOR WOMENS DOCUMENTS
+const WomenSchema=new mongoose.Schema({ 
+    col_sm_4href:String,
+    productImgTagsrc:String,
+    plp:String,
+    clr_shade_3:String,
+    clr_shade4:String,
+    wishlist_icon_animate_src:String,
+    discountedPriceText:String,
+    actualPriceText:String,
+    loyaltyPriceBox2:String,
+    d_flex:String
+})
+// WOMEN MODEL
+const ProductModelWomen=mongoose.model("women",WomenSchema);
+
+module.exports={ProductModelWomen,ProductModelMen};
