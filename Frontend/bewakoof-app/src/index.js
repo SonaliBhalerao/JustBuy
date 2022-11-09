a3207725e9ca38cfed9f528a1d5ac2fa440b87a0
SonaliBhalerao <sonalibhalerao120@gmail.com>
2022-11-09 11:25:32 +0530

    packages installed
    packages

--- Frontend/bewakoof-app/src/index.js
+++ Frontend/bewakoof-app/src/index.js
@@ -4,11 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react'
+import {BrowserRouter} from 'react-router-dom'
+import { store } from './Redux/store';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ChakraProvider>
-      <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </ChakraProvider>
 );
 
