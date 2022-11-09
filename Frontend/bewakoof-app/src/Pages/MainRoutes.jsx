7607265e3ba1f4f1fac16e0f0353c7a8d7e38e37
SonaliBhalerao <sonalibhalerao120@gmail.com>
2022-11-09 15:20:45 +0530

    MainRoutes updated
    MainRoutes

--- Frontend/bewakoof-app/src/Pages/MainRoutes.jsx
+++ Frontend/bewakoof-app/src/Pages/MainRoutes.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from "react-router-dom";
 
 const MainRoutes = () => {
   return (
-    <div>
-      hi
-    </div>
+    <Routes>
+        <Route />
+    </Routes>
   )
 }
 
