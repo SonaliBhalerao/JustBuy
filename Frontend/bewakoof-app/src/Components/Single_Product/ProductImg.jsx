3b652cb63a7f248420c2834d1798bc7358c685e2
heynaveed <naveedpasha0058@gmail.com>
2022-11-10 23:26:13 +0530

    Single Product Page

---
+++ Frontend/bewakoof-app/src/Components/Single_Product/ProductImg.jsx
@@ -0,0 +1,9 @@
+import { Box, Image } from "@chakra-ui/react";
+
+export default function SingleProductImg(){
+    return(
+        <Box width={"100%"}>
+            <Image margin={"auto"} height={"90%"} width={"95%"} src="https://images.bewakoof.com/t1080/men-s-white-what-the-duck-graphic-printed-oversized-t-shirt-553308-1667891931-1.jpg"/>
+        </Box>
+    )
+}
