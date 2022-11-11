729ea90facff7c548c004cad1230e98730831696
sand000 <kushwahasandhya68@gmail.com>
2022-11-11 23:00:49 +0530

    added filter and sorting routes in backend

--- Backend/Routes/AllProducts.route.js
+++ Backend/Routes/AllProducts.route.js
@@ -5,24 +5,62 @@ const AllProducts=Router();
 
 // ALL PRODUCTS FOR MENS
 AllProducts.get("/men",async(req,res)=>{
-    const Mens=await ProductModelMen.find({});
-    console.log(Mens);
-    res.send(Mens);
-})
+    let {category, finalprice, seller, rating, page, limit} = req.query; 
+    finalprice = Number(finalprice);
+    rating=Number(rating)
+    console.log(rating)
+    const Mens=await ProductModelMen.find({$or :[{},{category:category}]}).limit(limit).skip((page-1)*limit);  
+    console.log(category)
+    res.send(Mens); 
+});
+
+// SORT BY ASC BY PRICE MENS
+AllProducts.get("/men_asc",async(req,res)=>{
+    let {category, finalprice, seller, rating, page, limit} = req.query; 
+    finalprice = Number(finalprice)
+    const Mens=await ProductModelMen.find({}).sort({finalprice: 1}); 
+    res.send(Mens); 
+});
+
+// SORT BY DSC BY PRICE MENS
+AllProducts.get("/men_dsc",async(req,res)=>{
+    let {category, finalprice, seller, rating, page, limit} = req.query; 
+    finalprice = Number(finalprice)
+    const Mens=await ProductModelMen.find({}).sort({finalprice: -1}); 
+    res.send(Mens); 
+});
 
 // SINGLE PRODUCTS FOR MEN
 AllProducts.get("/men/:productid",async(req,res)=>{
     const payload=req.params.productid;
     const singledata=await ProductModelMen.findOne({_id:payload});
     res.send(singledata)
-})
+});
+
 
 // ALL PRODUCTS FOR WOMEN
 AllProducts.get("/women",async(req,res)=>{
-    const Womens=await ProductModelWomen.find({});
-    console.log(Womens);
-    res.send(Womens);
-})
+    let {category, finalprice, seller, rating, page, limit} = req.query; 
+    finalprice = Number(finalprice)
+    const Women=await ProductModelWomen.find({$or :[{},{category:category}]}).limit(limit).skip((page-1)*limit);   
+    res.send(Women); 
+});
+
+// SORT BY ASC BY PRICE WOMEN
+AllProducts.get("/women_asc",async(req,res)=>{
+    let {category, finalprice, seller, rating, page, limit} = req.query; 
+    finalprice = Number(finalprice)
+    const Women=await ProductModelWomen.find({}).sort({finalprice: 1}); 
+    res.send(Women); 
+});
+
+// SORT BY DSC BY PRICE WOMEN
+AllProducts.get("/women_dsc",async(req,res)=>{
+    let {category, finalprice, seller, rating, page, limit} = req.query; 
+    finalprice = Number(finalprice)
+    const Women=await ProductModelWomen.find({}).sort({finalprice: -1}); 
+    res.send(Women); 
+});
 
 
 // SINGLE PRODUCT FOR WOMEN
