c29d1039a24fb9f6ff2644ae0c8da96d55512019
GreatA-Maker <75976912+GreatA-Maker@users.noreply.github.com>
2022-11-12 23:53:54 +0530

    Cart products

--- Frontend/bewakoof-app/src/App.js
+++ Frontend/bewakoof-app/src/App.js
@@ -1,22 +1,16 @@
 import React from "react";
-import Cart from "./Components/Cart/Cart";
 
 import { Footer } from "./Components/footer/Footer";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { NavbarBottom } from "./Components/Navbar/navbar_bottom_menu/NavbarBottom.jsx";
-import LoginPage from "./Pages/Login_page/Login_page";
 
 import { MainRoutes } from "./Pages/MainRoutes.jsx";
-import { SinglePage } from "./Pages/Single_page/Singlepage";
 function App() {
 	return (
 		<div>
 			<Navbar />
-			{/* <NavbarBottom />
-			<MainRoutes /> */}
-			{/* <LoginPage/> */}
-			{/* <SinglePage /> */}
-			<Cart />
+			<NavbarBottom />
+			<MainRoutes />
 			<Footer />
 		</div>
 	);
