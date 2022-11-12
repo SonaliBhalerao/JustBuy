4c69224d405b97b0377306183526c7b2489c2527
sand000 <kushwahasandhya68@gmail.com>
2022-11-12 23:21:49 +0530

    frontend sorting and filtering

--- Frontend/bewakoof-app/src/App.js
+++ Frontend/bewakoof-app/src/App.js
@@ -1,21 +1,17 @@
 import React from "react";
 
 import { Footer } from "./Components/footer/Footer";
-import MensProductPage from "./Components/Men/MensProductPage";
 import { Navbar } from "./Components/Navbar/Navbar";
 import { NavbarBottom } from "./Components/Navbar/navbar_bottom_menu/NavbarBottom.jsx";
-import WomensProductPage from "./Components/Women/WomenProductPage";
-import LoginPage from "./Pages/Login_page/Login_page";
 
 import { MainRoutes } from "./Pages/MainRoutes.jsx";
-import { SinglePage } from "./Pages/Single_page/Singlepage";
+
 function App() {
 	return (
 		<div>
 			<Navbar />
-			<WomensProductPage/>
-			{/* <NavbarBottom />
-			<MainRoutes /> */}
+			<NavbarBottom />
+			<MainRoutes />
 			<Footer />
 		</div>
 	);
